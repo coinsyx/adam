c273b6b5118f94775f654159d45a8407515884b0
coinsyx <coinsyx@163.com>
2017-08-24 22:06:59 +0800

    add code

---
+++ src/Config.py
@@ -0,0 +1,14 @@
+
+class Config:
+    ################# global conf #######################
+    LOG_DIR = 'data'
+
+    ################# OkCoin ############################
+    OK_APIKEY = '2587ec5f-61a7-47f8-ae08-8449372b862c'
+    OK_SECRETKEY = '5E7073240E7A9BDFD326E15C4F55A83E'
+    OK_CN_RESTURL = 'www.okcoin.cn'
+
+    ################# TradeData #########################
+    TRADE_DATA_MAX_QUEUE_SIZE = 10240
+
+
